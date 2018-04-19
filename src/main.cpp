8fee19805c13bca5f2189422aa70aaec998cf74f
andimion666 <34198132+andimion666@users.noreply.github.com>
2018-04-19 17:20:35 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1073,10 +1073,10 @@ uint256 static GetOrphanRoot(const CBlockHeader* pblock)
 
 int64 static GetBlockValue(int nHeight, int64 nFees)
 {
-    int64 nSubsidy = 50 * COIN;
+    int64 nSubsidy = 125 * COIN;
 
-    // Subsidy is cut in half every 210000 blocks, which will occur approximately every 4 years
-    nSubsidy >>= (nHeight / 210000);
+    // Subsidy is cut in half every 400 blocks, which will occur approximately every 4 years
+    nSubsidy >>= (nHeight / 400);
 
     return nSubsidy + nFees;
 }
